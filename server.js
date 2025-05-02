98ba2695dc639f02a33139f5ff2cf4b38b3157ad
S.i RaNa <121483619+sirana858590@users.noreply.github.com>
2025-05-02 19:25:13 +0600

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,36 @@ app.post('/ShAn/imgur', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.json( 'Welcome To ShAn.s Api' );
+  const coloredText = `
+    <div style="font-family: monospace; font-size: 24px;">
+      <span style="color: #ff0000;">E</span>
+      <span style="color: #00ff00;">w</span>
+      <span style="color: #0000ff;">r</span>
+      <span> </span>
+      <span style="color: #ff00ff;">S</span>
+      <span style="color: #ffff00;">h</span>
+      <span style="color: #00ffff;">A</span>
+      <span style="color: #ff9900;">n</span>
+      <span style="color: #9900ff;">.</span>
+      <span style="color: #ff0099;">s</span>
+      <span> </span>
+      <span style="color: #00ff99;">A</span>
+      <span style="color: #ff6600;">p</span>
+      <span style="color: #6600ff;">i</span>
+      <span> </span>
+      <span style="color: #ff0066;">I</span>
+      <span style="color: #66ff00;">s</span>
+      <span> </span>
+      <span style="color: #0066ff;">R</span>
+      <span style="color: #ff3300;">u</span>
+      <span style="color: #3300ff;">n</span>
+      <span style="color: #ff0033;">n</span>
+      <span style="color: #33ff00;">i</span>
+      <span style="color: #0033ff;">n</span>
+      <span style="color: #ff0000;">g</span>
+    </div>
+  `;
+  res.send(coloredText);
 });
 
 const PORT = process.env.PORT || 3000;
