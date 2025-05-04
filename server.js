08bff97dae9845bfc6e53dd61b78bdf8e8a62aa0
S.i RaNa <121483619+sirana858590@users.noreply.github.com>
2025-05-04 14:53:31 +0600

    Update server.js

--- server.js
+++ server.js
@@ -35,8 +35,6 @@ app.post('/upload', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
   const coloredText = `
